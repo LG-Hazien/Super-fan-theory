3f3c3194b79da8cfbebb9859a1077ce6fe83b817
LG-Hazien <144539093+LG-Hazien@users.noreply.github.com>
2023-09-09 16:49:24 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "SuperFan";
+var name = "Super Fan Theory";
+var description = "Super Fan Theory";
+var authors = "LG-Hazien";
+var version = "1.0.0";
